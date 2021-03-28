b4b0a7273f5c212fe66acfc368ddf3ba822102c2
Little-White-PD <66425286+Little-White-PD@users.noreply.github.com>
2021-03-28 22:28:52 +0800

    Convertion
    Player can convert to infector from citizens

---
+++ StayHomeSafe/Assets/Buffs/ConvertToInfector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConvertToInfector : MonoBehaviour
+{
+
+    public GameObject citizen;
+    public Collider collider;
+
+    public void Start()
+    {
+        if (citizen.tag == "Infector")
+            collider.enabled = !collider.enabled;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Infector" && citizen.tag != "Infector")
+        {
+            citizen.tag = "Infector";
+            collider.enabled = !collider.enabled;
+        }
+    }
+}
