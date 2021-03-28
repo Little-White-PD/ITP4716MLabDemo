b170cef73a850f17abc98621d9f15aaefa543779
Little-White-PD <66425286+Little-White-PD@users.noreply.github.com>
2021-03-28 23:41:32 +0800

    Buff
    Turn to infector

--- StayHomeSafe/Assets/Buffs/ConvertToInfector.cs
+++ StayHomeSafe/Assets/Buffs/ConvertToInfector.cs
@@ -7,18 +7,28 @@ public class ConvertToInfector : MonoBehaviour
 
     public GameObject citizen;
     public Collider collider;
+    public bool HaveBuff;
+    public ParticleSystem buff;
 
     public void Start()
     {
         if (citizen.tag == "Infector")
+        {
+            HaveBuff = true;
             collider.enabled = !collider.enabled;
-    }
+            buff.Play();
+        }
+    }    
+ 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Infector" && citizen.tag != "Infector")
         {
+            HaveBuff = true;
             citizen.tag = "Infector";
             collider.enabled = !collider.enabled;
+            buff.Play();
         }
     }
+        
 }
