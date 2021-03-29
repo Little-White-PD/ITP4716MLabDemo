fb38ed2751c271607b08d83537f7b15ea20c5dad
AOMc2 <ho102791@gmail.com>
2021-03-29 13:42:55 +0800

    UI

---
+++ StayHomeSafe/Assets/Script/UI/SwitchScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+
+	public void NextScene(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+}
+
