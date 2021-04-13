e589272e7de2e0dd5469dadfcce58221ad697710
archunjai <archunjai@gmail.com>
2021-04-14 04:14:30 +0800

    npc

--- StayHomeSafe/Assets/Script/AI/PathFinding.cs
+++ StayHomeSafe/Assets/Script/AI/PathFinding.cs
@@ -10,6 +10,7 @@ public class PathFinding : MonoBehaviour
 	public float distance;
 	public int randComponent;
 	public int randLocation;
+	public Collider coll;
 
 
 	private void Awake()
@@ -24,20 +25,35 @@ public class PathFinding : MonoBehaviour
 		NM = GetComponent<NavMeshAgent>();
 		convert = GetComponentInChildren<ConvertToInfector>();
 		if (randComponent == 0)
+		{
 			convert.HaveBuff = true;
+		}
+
 		else
+		{
 			convert.HaveBuff = false;
+		}
 		
 	}
 
 	void Update()
-	{		
-		if (randLocation == 0)
-			NM.SetDestination(GameObject.FindWithTag("location1").transform.position);
-		else if(randLocation == 1)
-			NM.SetDestination(GameObject.FindWithTag("location2").transform.position);
+	{
+		if(convert.HaveBuff == true)
+		{
+
+			NM.SetDestination(GameObject.FindGameObjectWithTag("Player").transform.position);
+			StartCoroutine(time());
+			
+        }
 		else
-			NM.SetDestination(GameObject.FindWithTag("location3").transform.position);
+		{
+			if (randLocation == 0)
+				NM.SetDestination(GameObject.FindWithTag("location1").transform.position);
+			else if (randLocation == 1)
+				NM.SetDestination(GameObject.FindWithTag("location2").transform.position);
+			else
+				NM.SetDestination(GameObject.FindWithTag("location3").transform.position);
+		}
 	}
 
     private void OnTriggerEnter(Collider other)
@@ -45,5 +61,10 @@ public class PathFinding : MonoBehaviour
 		if (other.tag == "location1" || other.tag == "location2" || other.tag == "location3")
 			Destroy(gameObject);
     }
-	
+
+	IEnumerator time()
+	{
+		yield return new WaitForSeconds(7f);
+		Destroy(gameObject);
+	}
 }
