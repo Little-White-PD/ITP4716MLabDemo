45f4882bcc29a8253676a7ab85897679733de12c
archunjai <archunjai@gmail.com>
2021-04-14 03:37:58 +0800

    2

---
+++ StayHomeSafe/Assets/CarController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float time;
+    public Vector3 move;
+    public float moveSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //time += Time.deltaTime;
+        //if (time == 60)
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(-move * moveSpeed * Time.deltaTime);
+    }
+
+}
+
---
+++ StayHomeSafe/Assets/DesOJ.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesOJ : MonoBehaviour
+{
+    public Collider coll;
+
+    private void Start()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Wall")
+            Destroy(gameObject);
+
+    }
+}
