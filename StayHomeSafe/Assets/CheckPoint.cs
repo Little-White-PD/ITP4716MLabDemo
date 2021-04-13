ed6c9112a34e93378aacc2d42048036907dd5073
archunjai <archunjai@gmail.com>
2021-04-13 22:37:19 +0800

    1

---
+++ StayHomeSafe/Assets/CheckPoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    public GameObject[] checkPoint;
+    public Collider coll;
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        coll = GetComponent<Collider>();
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            checkPoint[0].SetActive(true);
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "location3")
+        {
+            checkPoint[0].SetActive(false);
+        }
+    }
+}
