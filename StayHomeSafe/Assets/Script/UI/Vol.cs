73e14f0b800345a069b6530dde5834a1f5a10370
AOMc2 <ho102791@gmail.com>
2021-04-19 12:24:57 +0800

    fix bug

---
+++ StayHomeSafe/Assets/Script/UI/Vol.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vol : MonoBehaviour
+{
+    public AudioSource AudioSource;
+    public float musicVol;
+    void Start()
+    {
+        musicVol = PlayerPrefs.GetFloat("BackgroundPref");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
