e326b08a51e9344401bcfbf04b0378d9f00bafb4
archunjai <archunjai@gmail.com>
2021-06-29 02:30:37 +0800

    open

---
+++ StayHomeSafe/Assets/Open.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Open : MonoBehaviour
+{
+    public KeyCode skip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(next());
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(skip))
+            SceneManager.LoadScene("Menu");
+    }
+
+    IEnumerator next() {
+        yield return new WaitForSeconds(6f);
+        SceneManager.LoadScene("Menu");
+    }
+}
